e30c4a90c82c53a563d6b00a627edd033cf1837b
Suchit <rajrikhy2@gmail.com>
2018-09-22 17:49:46 -0600

    chagnes

--- .profile
+++ .profile
@@ -3,9 +3,4 @@
 # add vendor binaries to the path
 # export PATH=$PATH:GIT_BRANCH=$(git branch)
 export "GIT_BRANCH_PROFILE=$(git branch)"
-export "GIT_BRANCH_PROFILE=$GIT_BRANCH"
-export LANG=${LANG:-en_US.UTF-8}
-
-export 1=$1
-export 2=$2
-export 3=$3
+export LANG1=${LANG:-en_US.UTF-8}
--- .profile.d/add-git-branch-env.sh
+++ .profile.d/add-git-branch-env.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-export "GIT_BRANCH_PROFILE1=$(git branch)"
-export LANG1=${LANG:-en_US.UTF-8}
-
-export TEST1=$1
-export TEST2=$2
-export TEST3=$3
-
-echo "GIT_BRANCH_PROFILE1: $GIT_BRANCH_PROFILE1"
-echo "LANG1 = $LANG1"
-
+export "GIT_BRANCH_PROFILE_TEST=$(git branch)"
+export LANG_TEST=${LANG:-en_US.UTF-8}
