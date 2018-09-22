70b90626a04bfbaf6454ae5161b64fbdbb44fbb3
Suchit <rajrikhy2@gmail.com>
2018-09-22 16:28:23 -0600

    stuff

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+console.log("BUILD_DIR", process.env.BUILD_DIR);
+console.log("CACHE_DIR", process.env.CACHE_DIR);
+console.log("SOURCE_VERSION", process.env.SOURCE_VERSION);
+console.log("STACK", process.env.STACK);
+
+console.log(JSON.stringify(process.env, null, 2));
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
