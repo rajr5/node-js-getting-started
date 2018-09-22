15c36ab8a6aba3df89594d2879f8bd8fc0178976
Suchit <rajrikhy2@gmail.com>
2018-09-22 16:34:25 -0600

    stuff

--- test-heroku.ts
+++ test-heroku.ts
@@ -1,3 +1,4 @@
+import { exec, execSync } from "child_process";
 import * as fs from "fs";
 
 console.log("BUILD_DIR", process.env.BUILD_DIR);
@@ -5,4 +6,7 @@ console.log("CACHE_DIR", process.env.CACHE_DIR);
 console.log("SOURCE_VERSION", process.env.SOURCE_VERSION);
 console.log("STACK", process.env.STACK);
 
-console.log(JSON.stringify(process.env, null, 2));
+process.env.GIT_DISCOVERY_ACROSS_FILESYSTEM = "1";
+
+const revision = execSync("git rev-parse --short HEAD");
+console.log("revision", revision);
