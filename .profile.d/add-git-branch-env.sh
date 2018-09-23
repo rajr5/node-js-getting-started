30a41a8e7d3f5b2a4c20e4bb7ddc81f9435b386b
Suchit <rajrikhy2@gmail.com>
2018-09-22 18:45:11 -0600

    changes

--- .profile.d/add-git-branch-env.sh
+++ .profile.d/add-git-branch-env.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-"GIT_BRANCH=$(git branch)"
 cat test.json
 $(sed -i'.bak' "s/_branch_/$GIT_BRANCH/g" test.json)
 cat test.json
 
-export GIT_BRANCH="$GIT_BRANCH"
 export TEST_VAR1=TEST
